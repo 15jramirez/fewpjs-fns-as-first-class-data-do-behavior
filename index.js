054f192f60c92409cf4d3d93d5369e6c896c34f2
jramirez <ramirez.jocelyn73@yahoo.com>
2021-06-30 21:03:13 -0700

    commit tree 196b8538751ec11800e5442ebbca4f5cfc7dd7eb

--- index.js
+++ index.js
@@ -11,3 +11,25 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+function greet(timeString){
+  let parsedTime = parseInt(timeString)
+
+  if (parsedTime < 12){
+    return "Good Morning"
+  }
+  else if (parsedTime >= 12 && parsedTime < 17){
+    return "Good Afternoon"
+  }
+
+  else if (parsedTime > 17 && parsedTime < 25){
+    return "Good Evening"
+  }
+  else{
+    return "Error. Please put time in milatry format"
+  }
+}
+
+function displayMessage(updatedGreeting){
+  let greeting = document.getElementById(`greeting`)
+  greeting.innerText = updatedGreeting
+}
